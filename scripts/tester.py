7c3374f5d440850784373928276b191727fab732
Izabela Kurek <ikurek0309@gmail.com>
2025-03-15 17:46:56 +0100

    tester functionality

--- scripts/tester.py
+++ scripts/tester.py
@@ -4,12 +4,11 @@ from tqdm import tqdm
 
 class MethodTester:
 
-    def __init__(self, solver: Solver):
+    def __init__(self):
         
-        self.solver = solver
         self.puzzle = Puzzle()
 
-    def test_solver(self):
+    def test_solver(self, solver: Solver):
         '''
         Performs a test of the solver by attempting to solve all puzzles.
         '''
@@ -19,8 +18,8 @@ class MethodTester:
         for id in tqdm(range(638)):
             try: 
                 random_words, id = self.puzzle.get_puzzle_by_id(id)
-                self.solver.solve_puzzle(random_words)
-                attempted_sol = self.solver.build_solution(random_words)
+                solver.solve_puzzle(random_words)
+                attempted_sol = solver.build_solution(random_words)
                 is_sol, num_corr = self.puzzle.check_solution(attempted_sol, id)
                 results["is_sol"].append(is_sol)
                 results["num_corr"].append(num_corr)
